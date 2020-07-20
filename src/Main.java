775dc6f00f1bbba291a1353807d26705aff18593
dmtevelavichius <tevelavichus@gmail.com>
2020-07-20 16:46:20 +0300

    Change src

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        long deposit = 2_000_000_000;
+                long transaction = 500_000_000;
+                        long total = deposit + transaction;
+                System.out.println(total);
+
+    }
+}
